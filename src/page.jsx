2886457aeba701cdcb5eedd1e3d7cabf8034be9f
bmansk8 <bmansk8@yahoo.com>
2020-07-04 13:57:10 -0400

    responsive
    fixed the grid system to make page more responsive.

--- src/page.jsx
+++ src/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './page.css';
 import blue1 from './photos/blue1.jpg';
 import green1 from './photos/green1.jpg';
 import purple1 from './photos/purple1.jpg';
@@ -13,6 +12,7 @@ import {
   Row,
   Col
 } from 'react-bootstrap'
+import './page.css';
 
 export default function Page() {
   return (
@@ -31,14 +31,14 @@ export default function Page() {
 
       <Container id='section' fluid>
         <Row>
-          <Col>
+          <Col sm={6}>
             <div className='collection rounded'>
               <h2>Contact Us</h2>
               <p>So while this website is a simple coding project for me, the guitars are real! If you are interested in buying one, just shoot me a email.
               I will try to get back to you as soon as I can.</p>
             </div>
           </Col>
-          <Col>
+          <Col sm={6}>
             <div id='about' className='rounded collection'>
               <h2>About Us</h2>
               <p>We have been in the music seen for over 30 years and got into building guitars in 2019. Since then
@@ -54,7 +54,7 @@ export default function Page() {
 
       <Container fluid id='guitars1'>
         <Row>
-          <Col>
+          <Col lg={4} md={6}>
             <Card>
               <Card.Img variant="top" src={blue1} />
               <Card.Body>
@@ -67,8 +67,8 @@ export default function Page() {
               </Card.Body>
             </Card>
           </Col>
-          <Col>
-            <Card >
+          <Col lg={4} md={6}>
+            <Card>
               <Card.Img variant="top" src={green1} />
               <Card.Body>
                 <Card.Title>F Hole Tele</Card.Title>
@@ -80,7 +80,7 @@ export default function Page() {
               </Card.Body>
             </Card>
           </Col>
-          <Col>
+          <Col lg={4} md={6}>
             <Card >
               <Card.Img variant="top" src={purple1} />
               <Card.Body>
@@ -94,47 +94,6 @@ export default function Page() {
             </Card>
           </Col>
         </Row>
-      </Container>
-
-      <Container fluid id='guitars2'>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={blue1} />
-            <Card.Body>
-              <Card.Title>Bigsby Tele <Badge variant='success'>New</Badge> </Card.Title>
-              <Card.Text>
-                This is our custom bigsy tremelo style tele! Sporting a amazing dark to light blue fade,
-                humbuckers, and F hole style body. this tele is warm and fun.
-                </Card.Text>
-              <Button href='/blue' variant="primary">Check me out!</Button>
-            </Card.Body>
-          </Card>
-
-          <Card >
-            <Card.Img variant="top" src={green1} />
-            <Card.Body>
-              <Card.Title>F Hole Tele</Card.Title>
-              <Card.Text>
-                This is our standard style F hole tele. Sporting light to dark green fade and single coils.
-                Get ready for a twangy, yet warm sound.
-                </Card.Text>
-              <Button href='/green' variant="primary">Check me out!</Button>
-            </Card.Body>
-          </Card>
-
-          <Card >
-            <Card.Img variant="top" src={purple1} />
-            <Card.Body>
-              <Card.Title>Texas Tele</Card.Title>
-              <Card.Text>
-                This is our texas blinged out tele. Sporting a wilkinson p90 in the neck, a alnicove single coil, bridge, and plate,
-                and that awesome purple burst finish.
-                </Card.Text>
-              <Button href='/purple' variant="primary" >Check me out!</Button>
-            </Card.Body>
-          </Card>
-
-        </Col>
       </Container>
 
     </div>
